bce9a9ae6d5f7547cf5e7ffde97cca6127cd81e1
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 02:24:17 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -131,5 +131,5 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages].push "Assembly" # << or [1] works too but push is pretty
+programmer_hash[:dennis_ritchie][:languages].push("Assembly") # << or [1] works too but push is pretty
 end
